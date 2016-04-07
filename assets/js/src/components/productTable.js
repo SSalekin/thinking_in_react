b94f45b7760bb77025b027050e0e09e86c6840ed
SSalekin <sjsalekin@gmail.com>
2016-04-07 15:21:17 +0600

    Gulp, Minify, Browserify

---
+++ assets/js/src/components/productTable.js
@@ -0,0 +1,33 @@
+var React = require('react');
+var ProductCategoryRow = require("./productCategoryRow");
+var ProductRow = require("./productRow");
+
+var ProductTable = React.createClass({
+  render: function() {
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach(function(product) {
+      if (product.name.toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1 || (!product.stocked && this.props.inStockOnly)) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    }.bind(this));
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+});
+
+module.exports = ProductTable;
