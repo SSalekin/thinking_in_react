7eb985d2f2bc176d9c742671fc3a8cc74a4a0bbd
dinhhoanglong91 <dinhhoanglong91@gmail.com>
2016-04-07 13:14:12 +0700

    Gulp by dinhhoanglong91

--- assets/js/src/components/searchbox.js
+++ assets/js/src/components/searchbox.js
@@ -1,3 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 var PRODUCTS = [
   {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
   {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
